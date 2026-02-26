49550fe7b34a7397d57b06aca9f0a5c168dc03a3
Davi Vieira Vitorino <davivvitorino@gmail.com>
2026-02-26 12:50:03 -0300

    Cria divs dentro da tela ("quadradinhos")

--- js/script.js
+++ js/script.js
@@ -5,6 +5,13 @@ let tamanho = document.getElementById('sizeInput')
 function criadorDeGrid(tamanho) {
     tela.style.gridTemplateColumns = `repeat(${tamanho}, 1fr)`;
     tela.style.gridTemplateRows = `repeat(${tamanho}, 1fr)`;
+
+    for (let i=0; i < tamanho * tamanho; i++) {
+        const quadradinho = document.createElement("div");
+        quadradinho.classList.add("quadradinho");
+        tela.appendChild(quadradinho);
+
+    }
 }
 
 tamanho.addEventListener('input', function () {
